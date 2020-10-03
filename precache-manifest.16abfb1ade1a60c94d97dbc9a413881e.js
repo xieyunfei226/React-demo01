d949c39ed237634782fd456d200a449cc403933c
xieyunfei226 <1625453189@qq.com>
2020-10-03 22:51:56 +0800

    demo01

---
+++ precache-manifest.16abfb1ade1a60c94d97dbc9a413881e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3953568ff271f41c4c76565fe2de19b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b32bd928cdda712f6696",
+    "url": "/static/js/2.1413a434.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1413a434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28fe4127e5ee7e00fcd5",
+    "url": "/static/js/main.a14a8ff6.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
